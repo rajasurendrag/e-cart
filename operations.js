cb1cc2d01ee7b0c9720b49ea93b03b3c3d65c87a
pradeepmoganti1307 <183721611+pradeepmoganti1307@users.noreply.github.com>
2024-12-28 22:29:19 +0530

    refactored

--- operations.js
+++ operations.js
@@ -1,8 +1,8 @@
-export const sort = (key, order, products) => {
+export const sort = (key, order, records) => {
   const asc = (object1, object2) => object1[key] - object2[key];
   const dsc = (object1, object2) => object2[key] - object1[key];
 
-  return products.toSorted(order === 1 ? dsc : asc);
+  return records.toSorted(order === 1 ? dsc : asc);
 };
 
 export const search = (key, value, records) => {
