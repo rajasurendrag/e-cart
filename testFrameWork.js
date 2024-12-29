7be027b0faa74faea7ca59ec009978995496f3bb
rajasurendrag <183709973+rajasurendrag@users.noreply.github.com>
2024-12-29 21:04:01 +0530

    updates testFrameWork

--- testFrameWork.js
+++ testFrameWork.js
@@ -4,18 +4,18 @@ const resultMessage = (actual, expected, func) => {
 
 const isEqual = (lhs, rhs) => lhs === rhs;
 
-const areObjectsEqual = (x, y) =>
-  areEqual(Object.entries(x).sort(), Object.entries(y).sort());
+const areObjectsEqual = (object1, object2) =>
+  areEqual(Object.entries(object1).sort(), Object.entries(object2).sort());
 
-const areEqual = (lhs, rhs) => {
-  const islhsAnArray = Array.isArray(lhs);
-  const islhsAnObject = !islhsAnArray && typeof lhs === "object";
+const areEqual = (entity1, entity2) => {
+  const isEntity1AnArray = Array.isArray(entity1);
+  const isEntity1AnObject = !isEntity1AnArray && typeof entity1 === "object";
 
-  if (islhsAnObject) return areObjectsEqual(lhs, rhs);
-  if (!islhsAnArray) return isEqual(lhs, rhs);
-  if (lhs.length !== rhs.length) return false;
+  if (isEntity1AnObject) return areObjectsEqual(entity1, entity2);
+  if (!isEntity1AnArray) return isEqual(entity1, entity2);
+  if (entity1.length !== entity2.length) return false;
 
-  return lhs.every((element, index) => areEqual(element, rhs[index]));
+  return entity1.every((element, index) => areEqual(element, entity2[index]));
 };
 
 const display = (result, func) => {
