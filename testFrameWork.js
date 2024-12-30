c65e20a8866d0e98a9aa04d9ab041b6a986cad52
rajasurendrag <183709973+rajasurendrag@users.noreply.github.com>
2024-12-30 23:31:47 +0530

    updates test frame work

--- testFrameWork.js
+++ testFrameWork.js
@@ -2,26 +2,23 @@ const resultMessage = (actual, expected, func) => {
   return func(actual, expected) ? "✔ passed" : "✘ failed";
 };
 
-const isEqual = (lhs, rhs) => lhs === rhs;
-
-const areObjectsEqual = (object1, object2) =>
-  areEqual(Object.entries(object1).sort(), Object.entries(object2).sort());
-
-const areEqual = (entity1, entity2) => {
-  const isEntity1AnArray = Array.isArray(entity1);
-  const isEntity1AnObject = !isEntity1AnArray && typeof entity1 === "object";
-
-  if (isEntity1AnObject) return areObjectsEqual(entity1, entity2);
-  if (!isEntity1AnArray) return isEqual(entity1, entity2);
-  if (entity1.length !== entity2.length) return false;
-
-  return entity1.every((element, index) => areEqual(element, entity2[index]));
-};
-
 const display = (result, func) => {
   console.log("testing :", func);
   console.table(result);
   console.log();
+};
+
+const areObjectsEqual = (obj1, obj2) => {
+  if (typeof obj1 !== "object") return obj1 === obj2;
+
+  const keysOfObj1 = Object.keys(obj1);
+  const keysOfObj2 = Object.keys(obj2);
+
+  if (keysOfObj1.length !== keysOfObj2.length) return false;
+
+  return keysOfObj1.every(
+    (key) => key in obj2 && areObjectsEqual(obj1[key], obj2[key])
+  );
 };
 
 export const testFunc = (func, tests) => {
